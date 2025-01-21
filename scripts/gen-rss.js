cf5d6924dc7e2fb5832af67361688ac780341940
雪沫子 <fdibdgsf@gmail.com>
2025-01-21 12:59:32 +0800

    更新 RSS feed 生成脚本

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'ORUNJI',
+    site_url: 'https://blog.onlyzzz.us.kg',
+    feed_url: 'https://blog.onlyzzz.us.kg/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
