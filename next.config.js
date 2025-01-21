a0d96ae203cf19c7c872a2fe1fa3020a89c63fd3
雪沫子 <fdibdgsf@gmail.com>
2025-01-21 20:04:33 +0800

    更改next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js');
+
+module.exports = withNextra({
+  images: {
+    domains: ['87c80b6.webp.li'],
+  },
+});
